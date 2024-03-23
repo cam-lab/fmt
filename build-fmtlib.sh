3ff55b1e8eac0bffc919297181b0536765f92829
hz <harry.zhurov@gmail.com>
2024-03-23 17:06:20 +0700

    add batch build script

---
+++ build-fmtlib.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cmake -H. -B_builds -DCMAKE_BUILD_TYPE=Release
+cmake --build _builds --config Release
+cmake -H. -B_builds -DCMAKE_INSTALL_PREFIX=.. -DCMAKE_BUILD_TYPE=Release
+cmake --build _builds --target install
